eab17e3907c4b6d44c8aab20775c126d92355256
yasminnabila <yasmnnabila@gmail.com>
2022-12-29 17:44:03 +0700

    Get users data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from "@nestjs/common";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+  @Get()
+  getUsers(): any {
+    return [{ id: 0 }];
+  }
+}
