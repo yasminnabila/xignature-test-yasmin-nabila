0cd9affe342ef23385264877d22f991d33b98a82
yasminnabila <yasmnnabila@gmail.com>
2022-12-31 00:52:26 +0700

    Install typeorm & sqlite

--- dist/app.module.js
+++ dist/app.module.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const users_module_1 = require("./users/users.module");
@@ -15,7 +16,7 @@ let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [users_module_1.UsersModule],
+        imports: [users_module_1.UsersModule, typeorm_1.TypeOrmModule.forRoot],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
