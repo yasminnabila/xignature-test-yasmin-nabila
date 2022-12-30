751b7f82aa622db890d6d89c8abec90897e44b8b
yasminnabila <yasmnnabila@gmail.com>
2022-12-30 08:51:56 +0700

    Configure DocumentBuilder of Nest API

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
+const swagger_1 = require("@nestjs/swagger");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle("Nest API")
+        .setDescription("Description of the API")
+        .setVersion("1.0")
+        .build();
     await app.listen(3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle("Nest API")
+    .setDescription("Description of the API")
+    .setVersion("1.0")
+    .build();
+
   await app.listen(3000);
 }
 bootstrap();
