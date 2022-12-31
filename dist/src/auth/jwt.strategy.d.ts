afb18f44659399a27c31dc335bfb63e0dc7b0d7c
yasminnabila <yasmnnabila@gmail.com>
2022-12-31 10:36:23 +0700

    Attempt on bearer jwt token

---
+++ dist/src/auth/jwt.strategy.d.ts
@@ -0,0 +1,6 @@
+import { Strategy } from "passport-local";
+declare const JwtStrategy_base: new (...args: any[]) => Strategy;
+export declare class JwtStrategy extends JwtStrategy_base {
+    constructor();
+}
+export {};
