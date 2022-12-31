8a95655d5eedacc888a5b39adb36b2eb35c165a8
yasminnabila <yasmnnabila@gmail.com>
2022-12-31 09:00:17 +0700

    Add UseGuard and return req.user in POST /login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { LocalAuthGuard } from "./auth/local-auth.guard";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   //? POST /login
+  @UseGuards(LocalAuthGuard)
   @Post("login")
-  login(): any {
-    return {};
+  login(@Request() req): any {
+    return req.user;
   }
 
   //? GET /protected
