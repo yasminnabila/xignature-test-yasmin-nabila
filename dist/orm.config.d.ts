d78cd4f8ce0515270862bdbd76ae792bd0105ea9
yasminnabila <yasmnnabila@gmail.com>
2022-12-31 07:23:12 +0700

    Create module and service for auth

--- dist/orm.config.d.ts
+++
@@ -1,3 +0,0 @@
-import { SqliteConnectionOptions } from "typeorm/driver/sqlite/SqliteConnectionOptions";
-declare const config: SqliteConnectionOptions;
-export default config;
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
+import { AuthModule } from './auth/auth.module';
 import config from "../orm.config";
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot(config)],
+  imports: [UsersModule, TypeOrmModule.forRoot(config), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
