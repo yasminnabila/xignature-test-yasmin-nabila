31a841d8b3c1a57bae2fdf0915049bff00aff126
yasminnabila <yasmnnabila@gmail.com>
2022-12-31 10:29:59 +0700

    Success getting access_token when log in a user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
-import { AppService } from "./app.service";
+import { AuthService } from "./auth/auth.service";
 import { AuthenticatedGuard } from "./auth/authenticated.guard";
 import { LocalAuthGuard } from "./auth/local-auth.guard";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post("login")
   login(@Request() req): any {
-    return { msg: "Logged in!" }; //? TODO: return JWT access token
+    return this.authService.login(req.user);
   }
 
   @UseGuards(AuthenticatedGuard)
